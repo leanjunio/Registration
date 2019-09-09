ad45c1b2616f783e5e8d29105c37bb954b6d6d11
Lean <leanjunio@live.com>
2019-09-09 13:31:59 -0400

    update backend for signup

--- api/signup.js
+++ api/signup.js
@@ -3,56 +3,46 @@ const bodyParser = require('body-parser');
 
 const User = require('../models/user');
 const Employee = require('../models/employee');
+const Administrator = require('../models/administrator');
 
 bodyParser.json();
 bodyParser.urlencoded({ extended: true });
 
 /**
- * Types for all the requested parameters:
- * firstName: String
- * lastName: String,
- * email: String,
- * password: String,
+ * Handles incoming requests regarding administrator sign up.
  * 
- * Note: It is best to use HTML validations to ensure that the data is validated prior to checking here
+ * NOTE: Email and password will be the primary source of authentication
  * 
- * This route should:
- * - look at the employees collection to check for the employeeId?
+ * When invoked, this route should:
+ * - Check whether the email already exists within the administrator collection
  */
 router.post('/signup', (req, res) => {
-  const validData = req.body.firstName && req.body.lastName && req.body.email && req.body.password && req.body.empId;
+  const validData = req.body.email && req.body.password;
   if (validData) {
+
+    // TODO: Find a mechanism to generate an employeeId that is 4 digits long and is unique in reference to the ones in the collection
     const userSignupData = {
-      empId: req.body.empId,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
       email: req.body.email,
       password: req.body.password
     }
 
-    // check if the user already exists
-    User.findOne({
-      firstName: userSignupData.firstName,
-      lastName: userSignupData.lastName,
-      lastName: userSignupData.lastName,
-    }, (err, user) => {
-      if (user) {
-        console.log(`User already signed up`);
+    // check if the email already exists in administrator collection
+    let query = Administrator.where({ email: userSignupData.email });
+
+    query.findOne((err, administrator) => {
+      if (err) throw err;
+
+      // If an administrator is found, the user is already registered therefore redirect to correct login
+      if (administrator) {
+        console.log(`Administrator already signed up`);
         res.redirect('./login');
-      }
-      else {
-        if (err) {
-          throw err;
-        } else {
-          // TODO: save information into another table from the database
-          User.create(userSignupData, (err, user) => {
-            if (err)
-              throw err;
-            else {
-              // TODO: Redirect to correct route when signup is successful
-            }
-          });
-        }
+      } else {
+        Administrator.create(userSignupData, (err, admin) => {
+          if (err) throw err;
+          else {
+            // TODO: Redirect to success route (dashboard??)
+          }
+        });
       }
     });
   } else {
---
+++ models/administrator.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const administratorSchema = new Schema({
+  email: String,
+  employeeId: Number,
+  password: String
+});
+
+module.exports = mongoose.model('Administrator', administratorSchema);

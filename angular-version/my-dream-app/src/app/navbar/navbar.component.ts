cac71e30f086db750027176d39d2c149c033af62
unknown <IvyLeano@outlook.com>
2019-09-10 12:02:24 -0400

    view for register done

--- angular-version/my-dream-app/src/app/navbar/navbar.component.ts
+++ angular-version/my-dream-app/src/app/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   navBarTitle = "Side Project One"; // you can change the title of the navbar here
+
   constructor() { }
 
   ngOnInit() {

85b0d8dacbee49497153d6ffb1a0663deaf6451b
Lean <leanjunio@live.com>
2019-09-13 14:07:50 -0400

    add change to email middleware

--- server/middleware/checks.js
+++ server/middleware/checks.js
@@ -3,7 +3,9 @@ const Administrator = require('../models/administrator');
 module.exports.checkEmail = async (req, res, next) => {
   const emailExists = await Administrator.findOne({ email: req.body.email });
   if (emailExists) {
-    return res.status(400).send(`A user with email: ${req.body.email} already exists`);
+    return res.status(400).send({ 
+      redirect: '/login'
+    });
   }
   next();
 }

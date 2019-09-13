7cd14f2603fe2e071d3e54730273824633c064d9
unknown <IvyLeano@outlook.com>
2019-09-13 11:42:17 -0400

    changed 4 methods to 1

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -13,23 +13,25 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit() {
   }
+
   // temporary methods for rendering the data
-  clickEmployees(){
-    //console.log("in getEmployees()");
-    this.navbarService.getEmployees()
-  }
-  clickTeams(){
-    //console.log("in getEmployees()");
-    this.navbarService.getTeams();
-  }
-  clickProjects(){
-    this.navbarService.getProjects();
-  }
-  clickPositions(){
-    this.navbarService.getPositions();
-  }
-  clickAdministrators(){
-    this.navbarService.getAdministrators();
+  // clickEmployees(){
+  //   this.navbarService.getEmployees()
+  // }
+  // clickTeams(){
+  //   this.navbarService.getTeams();
+  // }
+  // clickProjects(){
+  //   this.navbarService.getProjects();
+  // }
+  // clickPositions(){
+  //   this.navbarService.getPositions();
+  // }
+  // clickAdministrators(){
+  //   this.navbarService.getAdministrators();
+  // }
+  clickTab(path){
+    this.navbarService.getData(path);
   }
 
 }
--- client/src/app/navbar/navbar.service.ts
+++ client/src/app/navbar/navbar.service.ts
@@ -7,19 +7,22 @@ export class NavbarService {
 
   constructor() { }
 
-  getEmployees(){
-    console.log("entered getEmployees()");
-  }
-  getTeams(){
-    console.log("entered getTeams()");
-  }
-  getProjects(){
-    console.log("entered getProjects()");
-  }
-  getPositions(){
-    console.log("entered getPositions()");
-  }
-  getAdministrators(){
-    console.log("entered getAdministrators()");
+  // getEmployees(){
+  //   console.log("entered getEmployees()");
+  // }
+  // getTeams(){
+  //   console.log("entered getTeams()");
+  // }
+  // getProjects(){
+  //   console.log("entered getProjects()");
+  // }
+  // getPositions(){
+  //   console.log("entered getPositions()");
+  // }
+  // getAdministrators(){
+  //   console.log("entered getAdministrators()");
+  // }
+  getData(path){
+    console.log("entered " + path);
   }
 }

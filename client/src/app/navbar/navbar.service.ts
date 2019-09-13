59c6df598589797a4f53777f01bab0e41918f320
unknown <IvyLeano@outlook.com>
2019-09-13 12:27:06 -0400

    console.logs() data onClick of tabs

--- client/src/app/navbar/navbar.service.ts
+++ client/src/app/navbar/navbar.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavbarService {
-
-  constructor() { }
+  baseUrl = "https://glacial-shelf-53509.herokuapp.com/";
+  //constructor() { }
+  constructor(private httpClient: HttpClient){}
 
   // getEmployees(){
   //   console.log("entered getEmployees()");
@@ -22,7 +25,13 @@ export class NavbarService {
   // getAdministrators(){
   //   console.log("entered getAdministrators()");
   // }
+
+
   getData(path){
-    console.log("entered " + path);
+     console.log("entered getData(): " + this.baseUrl + path);
+     this.httpClient.get(this.baseUrl + path).subscribe((res)=>{
+            console.log(res);
+        });
+        //window.location.href = this.baseUrl + path;
   }
 }

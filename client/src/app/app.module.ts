f726712316ec8fae1b360afb3d8a67d6d3a50ae1
Lean <leanjunio@live.com>
2019-10-28 14:29:50 -0400

    added login component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     RegisterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -9,4 +9,5 @@ import { Component } from '@angular/core';
 export class NavbarComponent {
   navBarTitle = "Project: Registration";
   navBarTabOne = "Register";
+  navBarTabTwo = "Login";
 }

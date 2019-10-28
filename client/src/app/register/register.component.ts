eb5f7c3b053058e98aa485d88543b3e3846db417
Lean <leanjunio@live.com>
2019-10-28 07:27:53 -0400

    upload changes

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -28,7 +28,6 @@ export class RegisterComponent implements OnInit {
         this.success = true;
         this.receivedAdmin = admin;
         f.resetForm();
-      }     
-      );
+      });
   }
 }

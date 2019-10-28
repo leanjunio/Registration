864abb038858e7b9687b29c535ce6318b8e82fad
Lean <leanjunio@live.com>
2019-10-28 14:33:04 -0400

    login component ts

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Administrator } from '../administrator';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+  administrator: Administrator;
+  receivedAdmin: Administrator;
+  success = false;
+  fail = false;
+  errorMessage = ''
 
   constructor() { }
 
   ngOnInit() {
+    this.administrator = new Administrator('','');
   }
-
+  
+  onSubmit(f: NgForm) {
+    
+  }
 }

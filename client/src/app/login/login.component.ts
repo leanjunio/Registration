609b8fbe2fd5eefb879ccc944ac0e7fbc58d37c0
unknown <IvyLeano@outlook.com>
2019-11-04 14:19:08 -0500

    added some login functionality

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Administrator } from '../administrator';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+    administrator: Administrator;
+    receivedAdmin: Administrator;
+    success = false;
+    fail = false;
+    errorMessage = ''
+    
+    constructor() { }
+  
+    ngOnInit() {
+      this.administrator = new Administrator('', '');
+    }
+  
+    onSubmit(f: NgForm) {
+      console.log("login hit");
+    }
   }
+  
 
-}
+

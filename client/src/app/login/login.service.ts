8e6e84f41e74d2a23fe160b31e04eb80365b045f
unknown <IvyLeano@outlook.com>
2019-11-04 15:09:28 -0500

    added ToDo in login.service

--- client/src/app/login/login.service.ts
+++ client/src/app/login/login.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Administrator } from '../administrator';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  admins: Administrator[];
+  constructor(private administrator: Administrator) { 
 
-  constructor() { }
-
+   }
   authenticate(email, name){
-    console.log(email + " " + name);
+    //console.log(email + " " + name);
+    
+    //TODO: validate email and name in administrator database
   }
 
 }

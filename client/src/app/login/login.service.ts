f8f2f1c52bd97660235069c895e1283be92ef4dc
unknown <IvyLeano@outlook.com>
2019-11-11 17:15:55 -0500

    fixed catched error

--- client/src/app/login/login.service.ts
+++ client/src/app/login/login.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Login } from '../login';
 import { Observable } from 'rxjs';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
+import {catchError} from 'rxjs/operators/catchError'; 
 
 @Injectable({
   providedIn: 'root'
